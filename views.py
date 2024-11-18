97eebcb2ba6e7a86537c6e1ee937b02e51a7b566
Julllka <2biletavleto@mail.ru>
2024-11-18 15:29:08 +0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+import datetime
+import os
+
+def home_view(request):
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    # обратите внимание – здесь HTML шаблона нет, 
+    # возвращается просто текст
+    return HttpResponse(f'Time = {datetime.datetime.now().time()}')
+
+
+def workdir_view(request):
+    # по аналогии с `time_view`, напишите код,
+    # который возвращает список файлов в рабочей 
+    # директории
+    workdir_list = []
+    path = '.'
+    rez = os.listdir(path)
+    for n in rez:
+        workdir_list.append(f'{n}, ')
+    return HttpResponse(workdir_list)
+    #raise NotImplemented
